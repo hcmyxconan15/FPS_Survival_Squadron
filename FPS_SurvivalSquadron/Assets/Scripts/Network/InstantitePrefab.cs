b82d84f451a9d111c3d5cddc902939c4f53a372a
MSI\Phuc <hcmyxconan12@gmail.com>
2022-09-28 19:43:10 +0700

    Ui score photon

--- FPS_SurvivalSquadron/Assets/Scripts/Network/InstantitePrefab.cs
+++ FPS_SurvivalSquadron/Assets/Scripts/Network/InstantitePrefab.cs
@@ -6,13 +6,16 @@ using Photon.Pun;
 public class InstantitePrefab : MonoBehaviour
 {
     [SerializeField] GameObject prefab;
+    
     private void Start()
     {
         Instantite();
     }
-
+    
     private void Instantite()
     {
-        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+       PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+       
+        
     }
 }
