98351f8923fcdbbf12b854faa6416ec7e0d248a7
MSI\Phuc <hcmyxconan12@gmail.com>
2022-09-29 14:43:36 +0700

    fix player network

--- FPS_SurvivalSquadron/Assets/Scripts/Network/InstantitePrefab.cs
+++ FPS_SurvivalSquadron/Assets/Scripts/Network/InstantitePrefab.cs
@@ -8,6 +8,7 @@ public class InstantitePrefab : MonoBehaviour
 {
     [SerializeField] GameObject prefab;
     [SerializeField] Transform crossTarget;
+    [SerializeField] AmmoWidget ammoWidget;
 
     private void Start()
     {
@@ -18,5 +19,7 @@ public class InstantitePrefab : MonoBehaviour
     {
        GameObject go =  PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
        go.GetComponent<ActiveWeapon>().crossHairTarget = crossTarget;
+        go.GetComponent<ActiveWeapon>().ammoWidget = ammoWidget;
+        go.GetComponent<ReloadWeapon>().ammoWidget = ammoWidget;
     }
 }
