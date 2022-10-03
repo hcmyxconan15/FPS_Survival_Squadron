6d05c2f9eacc2cb0fdd96c9bb21437a3d38b26b8
baotran6112000 <baotran6112000@gmail.com>
2022-10-03 20:43:40 +0700

    update boss

---
+++ FPS_SurvivalSquadron/Assets/Scripts/AI/AttackEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEnemy : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform projectilePoint;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot()
+    {
+        Rigidbody rb = Instantiate(projectile, projectilePoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 30f, ForceMode.Impulse);
+        rb.AddForce(transform.up * 7, ForceMode.Impulse);
+    }
+}
