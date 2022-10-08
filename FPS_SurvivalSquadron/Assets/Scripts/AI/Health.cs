a6722716062492820b96439876a84caff6be501c
MSI\Phuc <hcmyxconan12@gmail.com>
2022-10-08 11:55:21 +0700

    fix health

--- FPS_SurvivalSquadron/Assets/Scripts/AI/Health.cs
+++ FPS_SurvivalSquadron/Assets/Scripts/AI/Health.cs
@@ -1,68 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public float maxHealth;
-    public float dieForce;
-    [HideInInspector] public float currentHealth;
-    Ragdoll ragdoll;
-    //UIHealthBar healthBar;
-    Animator animator;
-    public GameObject healthBar;
-    public GameObject borderHealth;
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        ragdoll = GetComponent<Ragdoll>();
-        currentHealth = maxHealth;
-        //healthBar = GetComponentInChildren<UIHealthBar>();
-        animator = GetComponent<Animator>();
-        var ridiBodies = GetComponentsInChildren<Rigidbody>();
-        foreach(var rigiBody in ridiBodies)
-        {
-            HitBox hitBox = rigiBody.gameObject.AddComponent<HitBox>();
-            hitBox.health = this;
-        }
-    }
+    [SerializeField]protected float maxHealth;
+    protected float dieForce;
+    protected float currentHealth;
+    public float MaxHealth => maxHealth;
+    public float DieForece => dieForce;
+    public float CurrentHealth => currentHealth;
 
     // Update is called once per frame
-    void Update()
-    {
-        SetHealth();
-    }
+    virtual public void TakeDamage(float amount, Vector3 direction) { }
 
-    void SetHealth()
-    {
-        healthBar.GetComponent<Image>().fillAmount = currentHealth / maxHealth;
-    }
-
-    public void TakeDamage(float amount, Vector3 direction)
-    {
-        currentHealth -= amount;
-        //healthBar.SetHealthBarPercentage(currentHealth / maxHealth);
-        Debug.Log("Current Health: " + currentHealth);
-        animator.SetTrigger("Hurt");
-        if(currentHealth <= 0.0f)
-        {
-            Die(direction);
-        }
-    }
-
-    void Die(Vector3 direction)
-    {
-        ragdoll.ActivateRagdoll();
-        direction.y = 1;
-        ragdoll.ApplyForce(direction * dieForce);
-        //healthBar.gameObject.SetActive(false);
-        healthBar.SetActive(false);
-        borderHealth.SetActive(false);
-        Destroy(gameObject,3f);
-    }
+    virtual public void Die(Vector3 direction) { }
 
      
 }
---
+++ FPS_SurvivalSquadron/Assets/Scripts/AI/HealthEnemy.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthEnemy : Health
+{
+    Ragdoll ragdoll;
+    //UIHealthBar healthBar;
+    Animator animator;
+    public GameObject healthBar;
+    public GameObject borderHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ragdoll = GetComponent<Ragdoll>();
+        currentHealth = maxHealth;
+        //healthBar = GetComponentInChildren<UIHealthBar>();
+        animator = GetComponent<Animator>();
+        var ridiBodies = GetComponentsInChildren<Rigidbody>();
+        foreach (var rigiBody in ridiBodies)
+        {
+            HitBox hitBox = rigiBody.gameObject.AddComponent<HitBox>();
+            hitBox.health = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetHealth();
+    }
+
+    void  SetHealth()
+    {
+        healthBar.GetComponent<Image>().fillAmount = currentHealth / maxHealth;
+    }
+
+    public override void  TakeDamage(float amount, Vector3 direction)
+    {
+        currentHealth -= amount;
+        //healthBar.SetHealthBarPercentage(currentHealth / maxHealth);
+        Debug.Log("Current Health: " + currentHealth);
+        animator.SetTrigger("Hurt");
+        if (currentHealth <= 0.0f)
+        {
+            Die(direction);
+        }
+    }
+
+    public override void Die(Vector3 direction)
+    {
+        ragdoll.ActivateRagdoll();
+        direction.y = 1;
+        ragdoll.ApplyForce(direction * dieForce);
+        //healthBar.gameObject.SetActive(false);
+        healthBar.SetActive(false);
+        borderHealth.SetActive(false);
+        Destroy(gameObject, 3f);
+    }
+}
